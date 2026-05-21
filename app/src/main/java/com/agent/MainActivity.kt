f67a31d9fac91434032b956ce9bdc2439dd308d1
Merlinbk611 <166020996+Merlinbk611@users.noreply.github.com>
2026-05-21 00:24:16 +0000

    add main activity

---
+++ app/src/main/java/com/agent/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.agent
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "Phone Agent Running"
+
+        setContentView(text)
+    }
+}
